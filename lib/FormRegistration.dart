20813e5c33b4b39ca3d1c382a8781a7faf3bb33f
rangga <rangga.alrasya@gmail.com>
2022-08-01 11:16:46 +0700

    Feat [RA] backbutton

--- lib/FormRegistration.dart
+++ lib/FormRegistration.dart
@@ -21,87 +21,91 @@ class _FormRegistrationState extends State<FormRegistration> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Form Registration')),
-      body: Container(
-        child: Column(children: [
-          SizedBox(height: 20),
-          Text(
-            "Rangdev Registration",
-            style: const TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          TextField(
-            controller: ctrusername,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Name',
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(children: [
+            SizedBox(height: 20),
+            Text(
+              "Rangdev Registration",
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
             ),
-          ),
-          Container(
-            height: 20,
-          ),
-          TextField(
-            controller: ctrpassword,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
+            SizedBox(
+              height: 40,
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Text("Pilih Jenis Kelamin", style: TextStyle(fontSize: 20)),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Radio(
-                value: 1,
-                groupValue: id,
-                onChanged: (value) {
-                  setState(() {
-                    id = 1;
-                  });
-                },
+            TextField(
+              controller: ctrusername,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name',
               ),
-              Text("Male"),
-            ],
-          ),
-          Row(
-            children: [
-              Radio(
-                value: 2,
-                groupValue: id,
-                onChanged: (value) {
-                  setState(() {
-                    id = 2;
-                  });
-                },
+            ),
+            Container(
+              height: 20,
+            ),
+            TextField(
+              controller: ctrpassword,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
               ),
-              Text("Female")
-            ],
-          ),
-          Text("Phone : " + myPhone),
-          ElevatedButton(
-            onPressed: () async {
-              final result = await Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SecondRoute()),
-              );
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text("Pilih Jenis Kelamin", style: TextStyle(fontSize: 20)),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: id,
+                  onChanged: (value) {
+                    setState(() {
+                      id = 1;
+                    });
+                  },
+                ),
+                Text("Male"),
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 2,
+                  groupValue: id,
+                  onChanged: (value) {
+                    setState(() {
+                      id = 2;
+                    });
+                  },
+                ),
+                Text("Female")
+              ],
+            ),
+            Text("Phone : " + myPhone),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondRoute()),
+                );
 
-              print("hasil input phone number : " + result.toString());
-              setState(() {
-                myPhone = result.toString();
-              });
-            },
-            child: Text("Edit Nomer Telepon"),
-            style: ElevatedButton.styleFrom(fixedSize: const Size(160, 20)),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text("Masukkan Data"))
-        ]),
+                print("hasil input phone number : " + result.toString());
+                setState(() {
+                  myPhone = result.toString();
+                });
+              },
+              child: Text("Edit Nomer Telepon"),
+              style: ElevatedButton.styleFrom(fixedSize: const Size(160, 20)),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Masukkan Data"))
+          ]),
+        ),
       ),
     );
   }
